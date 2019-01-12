633d0e37eb75803a56f60e604c918bc50f4d83eb
Le0nX <cloudmindone@gmail.com>
2019-01-12 19:21:04 +0300

    fix: added table cells

--- splitty/splitty/imageNames.swift
+++ splitty/splitty/imageNames.swift
@@ -15,8 +15,8 @@ var flowers = UIImage(named: "pexels-photo-305774.jpeg")!
 var stand   = UIImage(named: "pexels-photo-302588.jpeg")!
 var urban = UIImage(named: "pexels-photo-305243.jpeg")!
 
-
+var imageArray = [camera, city, animals, flowers, stand, urban]
 var nameArray = ["CAMERAS", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
 
-var colors = [#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1),]
+var colorsArray = [#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1),]
 
