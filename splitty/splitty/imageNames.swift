6789dade4b93425c135b4da3977755415b784c7d
Le0nX <cloudmindone@gmail.com>
2019-01-12 18:46:28 +0300

    init: project + data

---
+++ splitty/splitty/imageNames.swift
@@ -0,0 +1,22 @@
+//
+//  imageNames.swift
+//  splitty
+//
+//  Created by Denis Nefedov on 12/01/2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+var camera = UIImage(named: "pexels-photo-569995.png")!
+var city = UIImage(named: "pexels-photo-305254.jpeg")!
+var animals = UIImage(named: "pexels-photo-306036.jpeg")!
+var flowers = UIImage(named: "pexels-photo-305774.jpeg")!
+var stand   = UIImage(named: "pexels-photo-302588.jpeg")!
+var urban = UIImage(named: "pexels-photo-305243.jpeg")!
+
+
+var nameArray = ["CAMERAS", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+var colors = [#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1),#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1),#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1),]
+
